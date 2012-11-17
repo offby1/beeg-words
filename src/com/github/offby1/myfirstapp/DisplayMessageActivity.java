04cc73458b8bb5056c5aa1ed042490df1817a93f
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-17 12:04:24 -0800

    Done with http://developer.android.com/training/basics/firstapp/starting-activity.html

--- src/com/github/offby1/myfirstapp/DisplayMessageActivity.java
+++ src/com/github/offby1/myfirstapp/DisplayMessageActivity.java
@@ -1,10 +1,9 @@
 package com.github.offby1.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -12,8 +11,16 @@ public class DisplayMessageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-        // Show the Up button in the action bar.
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        Intent intent  = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
+    }
 }
