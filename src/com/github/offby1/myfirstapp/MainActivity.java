c3f95ecfdf2a36b5ad6b442c5e54e5681eb70f9d
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-17 13:18:53 -0800

    Nix the second activity, and the "Send" button; save text on every key event.

--- src/com/github/offby1/myfirstapp/MainActivity.java
+++ src/com/github/offby1/myfirstapp/MainActivity.java
@@ -2,17 +2,19 @@ package com.github.offby1.myfirstapp;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
     public final static String EXTRA_MESSAGE = "com.github.offby1.myfirstapp.MESSAGE";
 
     SharedPreferences sharedPref;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,27 @@ public class MainActivity extends Activity {
         sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
         String message = sharedPref.getString("message", "");
-        EditText editText = (EditText) findViewById(R.id.edit_message);
+        editText = (EditText) findViewById(R.id.edit_message);
         editText.setText(message);
+
+        editText.setOnKeyListener(new OnKeyListener() {
+
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    // Save the text on every keystroke!
+
+                    // TODO -- I suspect I'm saving everything twice
+                    // -- once on keyDown, and again on keyUp.  Fix
+                    // that.
+                    EditText editText = (EditText) v;
+                    String   message  = editText.getText().toString();
+
+                    SharedPreferences.Editor editor = sharedPref.edit();
+                    editor.putString("message", message);
+                    editor.commit();
+
+                    return false;
+                }
+            });
     }
 
     @Override
@@ -31,19 +52,5 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
-
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent   intent   = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String   message  = editText.getText().toString();
-
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("message", message);
-        editor.commit();
-
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
