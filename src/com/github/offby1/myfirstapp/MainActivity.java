c3bff8b3a00f1e4a36556fb9b4d6908f959ebe2a
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-11-17 12:49:09 -0800

    Cool!  I just added persistence.

--- src/com/github/offby1/myfirstapp/MainActivity.java
+++ src/com/github/offby1/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.github.offby1.myfirstapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,10 +12,18 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
     public final static String EXTRA_MESSAGE = "com.github.offby1.myfirstapp.MESSAGE";
 
+    SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+
+        String message = sharedPref.getString("message", "");
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        editText.setText(message);
     }
 
     @Override
@@ -28,6 +38,10 @@ public class MainActivity extends Activity {
         Intent   intent   = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String   message  = editText.getText().toString();
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("message", message);
+        editor.commit();
 
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
