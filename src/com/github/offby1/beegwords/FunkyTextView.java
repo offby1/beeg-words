09920ebc161b4181fe6b1d03dd89481c94522c10
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-12-08 09:13:22 -0800

    Create my own TextView class.
    Not sure what I'll do with it, but the project I'm
    imitating (https://github.com/catchthecows/BigTextButton) does
    something similar.

---
+++ src/com/github/offby1/beegwords/FunkyTextView.java
@@ -0,0 +1,19 @@
+package com.github.offby1.beegwords;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class FunkyTextView extends TextView {
+    public FunkyTextView(Context context) {
+        super (context);
+    }
+
+    public FunkyTextView(Context context, AttributeSet attrs) {
+        super (context, attrs);
+    }
+
+    public FunkyTextView(Context context, AttributeSet attrs, int defStyle) {
+        super (context, attrs, defStyle);
+    }
+}
