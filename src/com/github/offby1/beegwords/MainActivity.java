e737c544963db3d46073aa296f12d9aa6880dd80
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-12-19 20:11:48 -0800

    Nix TODO comments

--- src/com/github/offby1/beegwords/MainActivity.java
+++ src/com/github/offby1/beegwords/MainActivity.java
@@ -78,20 +78,20 @@ public class MainActivity extends Activity {
 
                 @Override
                 public void afterTextChanged(Editable s) {
-                    // TODO Auto-generated method stub
+                   
                     updateBeegWords(s.toString());
                 }
 
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count,
                                               int after) {
-                    // TODO Auto-generated method stub
+                   
                 }
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before,
                                           int count) {
-                    // TODO Auto-generated method stub
+                   
                 }
             });
 
