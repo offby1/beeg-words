7e6034a9016aaa78dfe293aad56755e65ce69935
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-10 12:16:37 -0700

    Nix unused imports

--- src/com/github/offby1/beegwords/MainActivity.java
+++ src/com/github/offby1/beegwords/MainActivity.java
@@ -9,14 +9,12 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
 
 @SuppressLint("DefaultLocale")
 public class MainActivity extends FragmentActivity {
