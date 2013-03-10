767ae90adaa364e7e4cf20f903a2de843a2d6b17
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-10 11:20:15 -0700

    Nix getPageTitle method; I don't need it

--- src/com/github/offby1/beegwords/MainActivity.java
+++ src/com/github/offby1/beegwords/MainActivity.java
@@ -86,19 +86,6 @@ public class MainActivity extends FragmentActivity {
             // Show 3 total pages.
             return 3;
         }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-            case 0:
-                return getString(R.string.title_section1).toUpperCase();
-            case 1:
-                return getString(R.string.title_section2).toUpperCase();
-            case 2:
-                return getString(R.string.title_section3).toUpperCase();
-            }
-            return null;
-        }
     }
 
     /**
